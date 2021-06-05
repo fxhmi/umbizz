496ff681014d1b76bc2897005081aa8daf34b3f4
fxhmi <50392574+fxhmi@users.noreply.github.com>
2021-06-05 15:16:55 +0800

    implemented firebase utils and sign up functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,7 +53,9 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
           <CustomButton type='submit'> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+          Sign in Google 
+          </CustomButton>
           </div>
       </form>
       </div>
