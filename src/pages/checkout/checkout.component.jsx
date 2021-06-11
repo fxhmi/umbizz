7ecc32e08197a8cfd8611fa4370d0c892b536b6b
fxhmi <50392574+fxhmi@users.noreply.github.com>
2021-06-12 02:48:53 +0800

    move the data collection to the firestore, add the spinner loading component when loading the data

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,11 +35,11 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: RM{total}</div>
-    <div className='test-warning'>
+    {/* <div className='test-warning'>
       *Please use the following test credit card for payments*
       <br />
       4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
-    </div>
+    </div> */}
     <StripeCheckoutButton price={total} />
 
   </div>
